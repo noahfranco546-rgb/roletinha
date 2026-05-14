051ac2328aaafe9fa10d32f006efce17cbcc2605
noah.franco <noah.franco@fulltime.com.br>
2026-05-14 17:13:10 -0300

    refactor: remove unused import of UserList

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from pokemonList import PokemonList
-from SortedList import SortedList
 from pathlib import Path
 
 app = FastAPI()
